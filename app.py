92c4026c825424a0fd0eb102285b8954aded70f5
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-08 12:16:11 +0700

    make it better

--- app.py
+++ app.py
@@ -33,8 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 import os
 if __name__ == "__main__":
