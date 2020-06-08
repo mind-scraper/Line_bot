b00149ac7cffcbb7d91a9ea60eb59b0dd4671044
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-08 12:24:37 +0700

    make it better

--- app.py
+++ app.py
@@ -33,8 +33,16 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
+"""
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+"""
+	
+	message = event.message.text
+    if message = "Hi" :
+		line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Hello, Sam.'))
+	 
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
