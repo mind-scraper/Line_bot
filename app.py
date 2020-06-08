30a2fe5bac500bcba248af66608be4764a9e4a0c
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-08 13:14:05 +0700

    make it better

--- app.py
+++ app.py
@@ -41,9 +41,8 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
 	
 #Repling spesific message
-    if message == "Hi":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Hello, Sam.'))
-
+#    if message == "Hi":
+#        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Hello, Sam.'))
 
 
 import os
