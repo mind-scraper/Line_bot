f17efd5dfbea32f34db3cc1b5571bc6757d75e05
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-08 19:44:48 +0700

    make it better

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def handle_message(event):
 	
 # Replying with spesific message
     if message == "Hi":
-        profile = line_bot_api.get_group_member_profile(text=event.source.group_id, text=event.source.user_id)
+        profile = line_bot_api.get_group_member_profile(event.source.group_id, event.source.user_id)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Halo, ' + profile.display_name))	
         
     if message == "cetak id":
