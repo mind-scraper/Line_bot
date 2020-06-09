4580298eac4cf7517f59cd3655a62bd74e543a94
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-09 22:56:49 +0700

    make it better

--- app.py
+++ app.py
@@ -57,11 +57,11 @@ def handle_message(event):
     for i in range(len(message)-1):
         a = message[i]+message[i+1]+message[i+2]+message[i+3]+message[i+4]+message[i+5]+message[i+6]
         if a == 'makasih' or a == 'Makasih':
-            if event.source.user_id == 'Udd20d357d3929f55680d1f989e99b6aa':
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Sama-sama, sayang.')
-            else:
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Sama-sama, ' + profile.display_name) + '.')
-            break        
+#            if event.source.user_id == 'Udd20d357d3929f55680d1f989e99b6aa':
+#                line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Sama-sama, sayang.')
+#            else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Sama-sama, ' + profile.display_name) + '.')
+        break        
     
 import os
 if __name__ == "__main__":
