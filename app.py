72b384cdecd4b98ecf5fda57c3441940e3e0baf6
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-12 23:51:45 +0700

    make it better

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def handle_message(event):
 
     #reply several words
     for i in range(len(message)-2):
-	    profile = line_bot_api.get_group_member_profile(event.source.group_id, event.source.user_id)
+        profile = line_bot_api.get_group_member_profile(event.source.group_id, event.source.user_id)
         a = message[i]+message[i+1]+message[i+2]
         if a == 'hi ' or a == 'Hi ':
             if event.source.user_id == id_sam:
