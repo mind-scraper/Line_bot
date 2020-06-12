0230f1ac25a9609924e22645a6b7c7ecae238316
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-13 00:18:12 +0700

    make it better

--- app.py
+++ app.py
@@ -100,18 +100,18 @@ def handle_message(event):
                 break
 
 #schedule
-def job():
-    line_bot_api.push_message(id_tesgrup, TextSendMessage(text='30 second has passed...'))
-schedule.every(0.5).minutes.do(job)
+#def job():
+#    line_bot_api.push_message(id_tesgrup, TextSendMessage(text='30 second has passed...'))
+#schedule.every(0.5).minutes.do(job)
 #schedule.every().hour.do(job)
 #schedule.every().day.at("10:30").do(job)
 #schedule.every(5).to(10).minutes.do(job)
 #schedule.every().monday.do(job)
 #schedule.every().wednesday.at("13:15").do(job)
 #schedule.every().minute.at(":17").do(job)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
 
 import os
 if __name__ == "__main__":
