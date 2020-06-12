873374196f7d9a8ae2ecef6b00a8fbb694fbe36b
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-12 10:13:46 +0700

    make it better

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def handle_message(event):
 
 # Replying with spesific message      
     if message[len(message)] == '?':
-	line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Ini pertanyaan.'))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Ini pertanyaan.'))
         query = message
         api_result = requests.get('http://api.serpstack.com/search?access_key=392fb6da2083ccf6427359826b72f2aa&query=' + query + '&engine=google&google_domain=google.co.id&page=1&output=json&%20location=surabaya')
         api_response = api_result.json()
