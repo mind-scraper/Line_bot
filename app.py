96aade437b3e82de90e3334041c0d15e8f9854c4
Samuel Payong Masan <spayongmasan@gmail.com>
2020-06-12 10:21:33 +0700

    make it better

--- app.py
+++ app.py
@@ -46,11 +46,11 @@ def handle_message(event):
         query = message
         api_result = requests.get('http://api.serpstack.com/search?access_key=392fb6da2083ccf6427359826b72f2aa&query=' + query + '&engine=google&google_domain=google.co.id&page=1&output=json&%20location=surabaya')
         api_response = api_result.json()
-
         for number, result in enumerate(api_response['organic_results']):
             if result['snippet'] != '':
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result['snippet']))
             break
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Operasi selesai.'))
 
     if message == "cetak id":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.source.user_id))
